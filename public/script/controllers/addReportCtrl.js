fab2d28a33c7ae0f9697db556bd0445124170cd3
oscarenriquez <oscar.enriquez.torre@gmail.com>
2016-09-30 10:43:44 -0600

    Commit

--- public/script/controllers/addReportCtrl.js
+++ public/script/controllers/addReportCtrl.js
@@ -4,19 +4,13 @@
         $scope.$watch(function() { return ($mdMedia('sm') || $mdMedia('xs')); }, function(big) {
             $scope.screenSize = big;
         });
-        $scope.changes = 0;
         $scope.report = {};
-        $scope.holaMundo = "Hola Mundo";
         $scope.closeDialog = function() {
             $mdDialog.cancel();
         };
         $scope.save = function() {
             $mdDialog.hide($scope.report)
-        }
-
-        $scope.$watch('report.name', function() {
-            $scope.changes++; 
-        });
+        };
     };
 
     angular.module('DashboardSL').controller('AddReportCtrl', DashboardSL.AddReportCtrl);
